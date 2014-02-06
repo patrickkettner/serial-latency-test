0ad95b548154a295448a9f646f0903beec20a8bd
Jakob Flierl <jakob.flierl@gmail.com>
2014-02-06 14:56:36 +0100

    update hr_timer.h

--- src/hr_timer.h
+++ src/hr_timer.h
@@ -24,40 +24,61 @@
 	#include <windows.h>
 	#pragma comment(lib, "kernel32.lib")
 	typedef LARGE_INTEGER timerStruct;
-	inline void GetHighResolutionTime( timerStruct *t ) 
-		{ QueryPerformanceCounter( t ); }
-	inline double ConvertTimeDifferenceToSec( timerStruct *end, timerStruct *begin ) 
-		{ timerStruct freq; QueryPerformanceFrequency( &freq );  return (end->QuadPart - begin->QuadPart)/(double)freq.QuadPart; }
+
+    inline void GetHighResolutionTime(timerStruct *t) {
+		QueryPerformanceCounter(t);
+	}
+
+	inline double ConvertTimeDifferenceToSec(timerStruct *end, timerStruct *begin) {
+		timerStruct freq;
+
+		QueryPerformanceFrequency(&freq);
+
+		return (end->QuadPart - begin->QuadPart) / (double)freq.QuadPart;
+	}
 
 #elif defined(USING_MINGW)
 
 	#include <windows.h>
 	typedef LARGE_INTEGER timerStruct;
-	inline void GetHighResolutionTime( timerStruct *t ) 
-		{ QueryPerformanceCounter( t ); }
-	inline double ConvertTimeDifferenceToSec( timerStruct *end, timerStruct *begin ) 
-		{ timerStruct freq; QueryPerformanceFrequency( &freq );  return (end->QuadPart - begin->QuadPart)/(double)freq.QuadPart; }
+
+	inline void GetHighResolutionTime(timerStruct *t) {
+		QueryPerformanceCounter(t);
+	}
+
+	inline double ConvertTimeDifferenceToSec(timerStruct *end, timerStruct *begin) {
+		timerStruct freq;
+
+		QueryPerformanceFrequency(&freq);
+		
+		return (end->QuadPart - begin->QuadPart) / (double)freq.QuadPart;
+	}
 
 #elif defined(USING_LINUX)  // Assume we have POSIX calls clock_gettime() 
 	#include <time.h>
 	typedef struct timespec timerStruct;
-	inline void GetHighResolutionTime( timerStruct *t ) 
-		{
-#if defined(CLOCK_MONOTONIC_RAW)
-			clock_gettime( CLOCK_MONOTONIC_RAW, t );
-#else
-			clock_gettime( CLOCK_MONOTONIC, t );
-#endif
-		}
-	inline double ConvertTimeDifferenceToSec( timerStruct *a, timerStruct *b ) 
-	{
-		unsigned int diff;
 
-		if (a->tv_sec - b->tv_sec > 2)
-			return UINT_MAX / (double)1000000000.0;
-		diff = (a->tv_sec - b->tv_sec) * 1000000000;
-		diff += a->tv_nsec - b->tv_nsec;
-		return (double)diff / (double)1000000000.0;
+    inline void GetHighResolutionTime(timerStruct *t) {
+#if defined(CLOCK_MONOTONIC_RAW)
+		clock_gettime(CLOCK_MONOTONIC_RAW, t);
+#else
+		clock_gettime(CLOCK_MONOTONIC, t);
+#endif
+	}
+
+	inline double ConvertTimeDifferenceToSec(timerStruct *end, timerStruct *begin) {
+
+		timerStruct temp;
+
+		if ((end->tv_nsec-begin->tv_nsec)<0) {
+			temp.tv_sec = end->tv_sec-begin->tv_sec-1;
+			temp.tv_nsec = 1000000000+end->tv_nsec-begin->tv_nsec;
+		} else {
+			temp.tv_sec = end->tv_sec-begin->tv_sec;
+			temp.tv_nsec = end->tv_nsec-begin->tv_nsec;
+		}
+
+		return (temp.tv_sec) + (1e-9)*(temp.tv_nsec); 
 	}
 
 #elif defined(USING_MACOSX)  // Assume we're running on MacOS X
@@ -70,12 +91,20 @@
 	#include <CoreServices/CoreServices.h>
 	#include <mach/mach.h>
 	#include <mach/mach_time.h>
+
 	typedef uint64_t timerStruct;
-	inline void GetHighResolutionTime( timerStruct *t ) 
-		{ *t = mach_absolute_time(); }
-	inline double ConvertTimeDifferenceToSec( timerStruct *end, timerStruct *begin ) 
-		{ uint64_t elapsed = *end - *begin; Nanoseconds elapsedNano = AbsoluteToNanoseconds( *(AbsoluteTime*)&elapsed );
-			return double(*(uint64_t*)&elapsedNano) * (1e-9); }
+
+	inline void GetHighResolutionTime(timerStruct *t) {
+		*t = mach_absolute_time();
+	}
+
+	inline double ConvertTimeDifferenceToSec(timerStruct *end, timerStruct *begin) {
+		uint64_t elapsed = *end - *begin;
+
+		Nanoseconds elapsedNano = AbsoluteToNanoseconds(*(AbsoluteTime*)&elapsed);
+
+		return double(*(uint64_t*)&elapsedNano) * (1e-9);
+	}
 #endif
 
 #endif // end #ifndef HR_TIMER_H
